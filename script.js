23886f5e8fe980b49e8b59cd42379bfc6bd30ac3
Promasterio <108183007+Promasterio@users.noreply.github.com>
2024-05-19 23:15:32 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function showAlert() {
+    alert("You can contact me at john.doe@example.com or (123) 456-7890.");
+}
+
+function scrollToDetails() {
+    document.getElementById("details").scrollIntoView({ behavior: "smooth" });
+}
